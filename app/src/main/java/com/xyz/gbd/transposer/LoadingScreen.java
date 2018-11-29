4d0a0fff50e87e2bc0320f81954bc35d7a12b2fd
Gurnek Mokha <gmokha2@illinois.edu>
2018-11-28 21:53:46 -0600

    Added basic animations

--- app/src/main/java/com/xyz/gbd/transposer/LoadingScreen.java
+++ app/src/main/java/com/xyz/gbd/transposer/LoadingScreen.java
@@ -5,13 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoadingScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        //Animation to fade in sum stuff
+        final Animation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(3000);
+
+        final Animation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+        fadeOut.setDuration(3000);
+
+        TextView logo = findViewById(R.id.logo);
+        logo.startAnimation(fadeIn);
     }
 
     public void switchScreen(View view) {
--- app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
+++ app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
@@ -2,16 +2,12 @@ package com.xyz.gbd.transposer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.media.MediaPlayer;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    ImageView iv = (ImageView) findViewById(R.id.staff);
 }
