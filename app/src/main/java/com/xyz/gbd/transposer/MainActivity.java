6baf7f04f1ebba31cb136b7eabdff46b5efb4d31
danielgleason <dlg2@illinois.edu>
2018-12-01 16:45:31 -0600

    getSteps finished

--- app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
+++ app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
@@ -3,21 +3,31 @@ package com.xyz.gbd.transposer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    protected Spinner begin;
+    protected Spinner end;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner begin = findViewById(R.id.original_key);
-        Spinner end = findViewById(R.id.transpose_key);
+        begin = findViewById(R.id.original_key);
+        end = findViewById(R.id.transpose_key);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.key_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         begin.setAdapter(adapter);
         end.setAdapter(adapter);
     }
+
+    public void transposeClicked(View view) {
+        String startKey = begin.getSelectedItem().toString();
+        String endKey = end.getSelectedItem().toString();
+    }
+
 }
