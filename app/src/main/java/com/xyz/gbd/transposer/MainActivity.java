1091b33fd7dd81c479452b1e1dd5edfba5e7149d
Gurnek Mokha <gmokha2@illinois.edu>
2018-12-01 16:46:04 -0600

    Added fidget spinners

--- app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
+++ app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
@@ -2,7 +2,6 @@ package com.xyz.gbd.transposer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.media.MediaPlayer;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner begin = findViewById(R.id.original_key);
         Spinner end = findViewById(R.id.transpose_key);
+
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.key_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
---
+++ lib/src/main/java/com/xyz/gbd/lib/SansMachine.java
@@ -0,0 +1,5 @@
+package com.xyz.gbd.lib;
+
+public class SansMachine {
+
+}
