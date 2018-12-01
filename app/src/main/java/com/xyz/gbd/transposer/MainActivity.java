2e06572a8aca96f5738aa9054e5f17af620e5cf9
bryant <bryanth2@illinois.edu>
2018-12-01 17:43:41 -0600

    Drag Stuff more

--- app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
+++ app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
@@ -1,10 +1,16 @@
 package com.xyz.gbd.transposer;
-
+import java.lang.Object;
+import android.os.Parcelable.Creator;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+
+import static java.lang.System.out;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -21,7 +27,53 @@ public class MainActivity extends AppCompatActivity {
         begin.setAdapter(adapter);
         end.setAdapter(adapter);
     }
+<<<<<<< HEAD
+    public interface Parcelable {
+        public class MyParcelable implements Parcelable {
+            private int mData;
+
+            public int describeContents() {
+                return 0;
+            }
+            public void writeToParcel(Parcel out, int flags) {
+                out.writeInt(mData);
+            }
+            public static final Parcelable.Creator<MyParcelable> CREATOR
+                    = new Parcelable.Creator<MyParcelable>() {
+                public MyParcelable createFromParcel(Parcel in) {
+                    return MyParcelable(in);
+                }
+                public MyParcelable[] newArray(int size) {
+                    return MyParcelable[size];
+                }
+            } ;
+            private MyParcelable(Parcel in) {
+                mData = in.readInt();
+            }
+        }
+    }
+    /*
+    For reference
+    "https://developer.android.com/reference/android/view/DragEvent"
+     */
+    public class  DragEvent extends Object implements Parcelable {
+        public static final int ACTION_DRAG_STARTED;
+        public static final int ACTION_DRAG_DROP;
+        public static final int ACTION_DRAG_ENDED;
+        public static final int ACTION_DRAG_ENTERED;
+        public static final int ACTION_DRAG_EXITED;
+        public static final int ACTION_DRAG_LOCATION;
+        public int describeContents() {
+            return
+        }
+        public int getAction() {
+            return
+        }
+        public float getY() {
+            return
+        }
 
     public void PlaySound(View view) {
+
     }
-}
+
