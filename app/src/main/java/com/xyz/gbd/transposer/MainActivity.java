246cf683dbbc8f68466000bde3746a926b073400
danielgleason <dlg2@illinois.edu>
2018-12-06 13:31:39 -0600

    key sigs first pass

--- app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
+++ app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
@@ -39,18 +39,78 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         String key = currentKey;
         switch (key) {
             case "C" :
-
+                changeFlats(0);
+                changeSharps(0);
+                break;
+            case "F" :
+                changeFlats(1);
+                changeSharps(0);
+                break;
+            case "Bb" :
+                changeFlats(2);
+                changeSharps(0);
+                break;
+            case "Eb" :
+                changeFlats(3);
+                changeSharps(0);
+                break;
+            case "Ab" :
+                changeFlats(4);
+                changeSharps(0);
+                break;
+            case "Db" :
+                changeFlats(5);
+                changeSharps(0);
+                break;
+            case "Gb" :
+                changeFlats(6);
+                changeSharps(0);
+                break;
+            case "Cb" :
+                changeFlats(7);
+                changeSharps(0);
+                break;
+            case "G" :
+                changeFlats(0);
+                changeSharps(1);
+                break;
+            case "D" :
+                changeFlats(0);
+                changeSharps(2);
+                break;
+            case "A" :
+                changeFlats(0);
+                changeSharps(3);
+                break;
+            case "E" :
+                changeFlats(0);
+                changeSharps(4);
+                break;
+            case "B" :
+                changeFlats(0);
+                changeSharps(5);
+                break;
+            case "F#" :
+                changeFlats(0);
+                changeSharps(6);
+                break;
+            case "C#" :
+                changeFlats(0);
+                changeSharps(7);
+                break;
         }
 
     }
 
-    private void hideAllKeys() {
-        ImageView bflat = findViewById(R.id.bflat);
-        bflat.setVisibility(View.GONE);
-    }
     private void changeFlats(int numFlats) {
-        String[] flats = new String[] {"b", "e", "a", "d", "g", "c", "f"};
+        String[] flats = new String[]{"b", "e", "a", "d", "g", "c", "f"};
         for (int i = 0; i < numFlats; i++) {
+            String id = "@+id/" + flats[i] + "flat";
+            int currentFlat = getResources().getIdentifier(id, "layout", null);
+            ImageView thisFlat = findViewById(currentFlat);
+            thisFlat.setVisibility(View.VISIBLE);
+        }
+        for (int i = numFlats; i < 7; i++) {
             String id = "@+id/" + flats[i] + "flat";
             int currentFlat = getResources().getIdentifier(id, "layout", null);
             ImageView thisFlat = findViewById(currentFlat);
@@ -58,7 +118,24 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
     }
 
+    private void changeSharps(int numSharps) {
+        String[] sharps = new String[] {"f", "c", "g", "d", "e", "a", "b"};
+        for (int i = 0; i < numSharps; i++) {
+            String id = "@+id/" + sharps[i] + "sharp";
+            int currentSharp = getResources().getIdentifier(id, "layout", null);
+            ImageView thisSharp = findViewById(currentSharp);
+            thisSharp.setVisibility(View.VISIBLE);
+        }
+        for (int i = numSharps; i < 7; i++) {
+            String id = "@+id/" + sharps[i] + "sharp";
+            int currentSharp = getResources().getIdentifier(id, "layout", null);
+            ImageView thisSharp = findViewById(currentSharp);
+            thisSharp.setVisibility(View.GONE);
+        }
+    }
+
     private boolean moving = false;
+
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         switch (event.getAction()) {
@@ -73,16 +150,16 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                     float y = event.getRawY() - wholeNote.getHeight() * 3 / 2;
                     //Third: Constantly check if the cursor's pos is 20 or more different than note
                     /**
-                    if (Math.abs(notePos - y) >= 20) {
-                        //Fourth: if it is, move the note and set the new position as step 1
-                        if (y > notePos) {
-                            wholeNote.setY(notePos + 20);
-                            notePos += 20;
-                        } else {
-                            wholeNote.setY(notePos - 20);
-                            notePos -= 20;
-                        }
-                    }
+                     if (Math.abs(notePos - y) >= 20) {
+                     //Fourth: if it is, move the note and set the new position as step 1
+                     if (y > notePos) {
+                     wholeNote.setY(notePos + 20);
+                     notePos += 20;
+                     } else {
+                     wholeNote.setY(notePos - 20);
+                     notePos -= 20;
+                     }
+                     }
                      */
                     wholeNote.setY(y);
                 }
