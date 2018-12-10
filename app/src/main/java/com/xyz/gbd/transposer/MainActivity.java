4053c1fd613c7af4627a43d04f944323f7bad796
Gurnek Mokha <gmokha2@illinois.edu>
2018-12-10 14:59:56 -0600

    snapy kinda workd

--- app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
+++ app/src/main/java/com/xyz/gbd/transposer/MainActivity.java
@@ -1,16 +1,18 @@
 package com.xyz.gbd.transposer;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
-
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
     ImageView wholeNote;
+    ImageView staff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +30,19 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         wholeNote = findViewById(R.id.wholenote);
         wholeNote.setOnTouchListener(this);
+        staff = findViewById(R.id.staff);
     }
 
-    private float snapY(float rawY) {
-        return 0;
+    private float snapY() {
+        int staffHeight = staff.getHeight();
+        float staffY = staff.getY();
+        float wholeNoteY = wholeNote.getY();
+        float percentDown = (wholeNoteY - staffY) / staffHeight;
+        if (Math.ceil(percentDown / 0.07) - (percentDown / 0.07) > Math.floor(percentDown / 0.07) - (percentDown / 0.07)) {
+            return (float) (staffHeight * Math.ceil(percentDown / 0.07) / 100 + staffY);
+        } else {
+            return (float) (staffHeight * Math.floor(percentDown / 0.07) / 100 + staffY);
+        }
     }
 
     private boolean moving = false;
@@ -43,12 +54,14 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 break;
             case MotionEvent.ACTION_MOVE:
                 if (moving) {
-                    float y = event.getRawY() - wholeNote.getHeight() * 3 / 2;
-                    wholeNote.setY(y);
+                    double y = event.getRawY() - wholeNote.getHeight() * 3 / 2;
+                    wholeNote.setY((float) y);
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 moving = false;
+                wholeNote.setY(snapY());
+                Log.d("Fucking bitchwad", Float.toString(wholeNote.getY()));
                 break;
         }
         return true;
